f4dfdbf709731933a8ee75cb68cf9534b5961eb5
Your Name <you@example.com>
2017-01-05 17:40:30 +0900

    1/5

---
+++ Java/workspace_20170105/PlayBascketball/src/bascketball/Main.java
@@ -0,0 +1,13 @@
+package bascketball;
+
+public class Main {
+public static void main(String[] args)
+{
+	GameSet bascketball = new GameSet();
+	bascketball.start();
+      
+	
+	  
+
+}
+}
---
+++ Java/workspace_20170105/constructor_2/src/constructor_2/Position.java
@@ -0,0 +1,25 @@
+package constructor_2;
+
+class Position {
+
+	int x;
+	int y;
+	
+	public Position()
+	{
+		x=0;
+		y=0;
+		System.out.println("³ª¿Í¶ó");
+	}
+	public Position(int n,int k)
+	{
+		x=n;
+		y=k;
+	}
+	public Position(int g)
+	{
+		x=g;
+		y=g;
+	}
+}
+	
